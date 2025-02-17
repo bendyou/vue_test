caaebef9f2da06de9e6579cbc782d4a0c46aa920
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:14:41 +0300

    Initial commit in task_24

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div>
-    <p v-if="day === 'Monday'">Monday</p>
-    <p v-if="day === 'Tuesday'">Tuesday</p>
-    <p v-if="day === 'Wednesday'">Wednesday</p>
-    <p v-if="day === 'Thursday'">Thursday</p>
-    <p v-if="day === 'Friday'">Friday</p>
-    <p v-if="day === 'Saturday'">Saturday</p>
-    <p v-if="day === 'Sunday'">Sunday</p>
-  </div>
+  <p v-if="age < 18">teenager</p>
+  <p v-else-if="age >= 19 && age <= 25">young man</p>
+  <p v-else>man</p>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      day: 'Wednesday',
+      age: 25,
     };
   },
 };
 </script>
-
-<style scoped>
-p {
-  font-size: 18px;
-  margin-top: 10px;
-}
-</style>
