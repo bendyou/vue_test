0bc0798777dbaaf59af1446760bc6abcc70a0f20
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:21:34 +0300

    Initial commit in task_31

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
     <h1>Task 1</h1>
-    <div v-for="elem in items" :key="elem">
-      <p>{{ elem }}</p>
-      <p class="divider"></p>
-    </div>
+    <p v-for="user in users" :key="user.name">
+      {{ user.name }} {{ user.surn }}
+    </p>
 
     <h1>Task 2</h1>
     <ul>
-      <li v-for="(elem, index) in items" :key="elem">
-        {{ elem }}
+      <li v-for="href in hrefs" :key="href.href">
+        <a :href="href.href">{{ href.text }}</a>
       </li>
-      <li v-for="(elem, index) in items" :key="'divider' + index" class="divider"></li>
     </ul>
+
+    <h1>Task 3</h1>
+    <table>
+      <tr v-for="product in products" :key="product.name">
+        <td>{{ product.name }}</td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.quantity }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -20,7 +27,21 @@
 export default {
   data() {
     return {
-      items: [1, 2, 3]
+      users: [
+        { name: 'name1', surn: 'surn1' },
+        { name: 'name2', surn: 'surn2' },
+        { name: 'name3', surn: 'surn3' }
+      ],
+      hrefs: [
+        { href: '1.html', text: 'text1' },
+        { href: '2.html', text: 'text2' },
+        { href: '3.html', text: 'text3' }
+      ],
+      products: [
+        { name: 'product1', price: 100, quantity: 5 },
+        { name: 'product2', price: 200, quantity: 4 },
+        { name: 'product3', price: 300, quantity: 3 }
+      ]
     };
   }
 };
