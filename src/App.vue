0eafbf61922983cebef58fc93b8c1f07c74a71d7
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:21:55 +0300

    Initial commit in task_32

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,14 @@
 <template>
   <div>
     <h1>Task 1</h1>
-    <p v-for="user in users" :key="user.name">
-      {{ user.name }} {{ user.surn }}
-    </p>
-
-    <h1>Task 2</h1>
-    <ul>
-      <li v-for="href in hrefs" :key="href.href">
-        <a :href="href.href">{{ href.text }}</a>
-      </li>
-    </ul>
-
-    <h1>Task 3</h1>
-    <table>
-      <tr v-for="product in products" :key="product.name">
-        <td>{{ product.name }}</td>
-        <td>{{ product.price }}</td>
-        <td>{{ product.quantity }}</td>
-      </tr>
-    </table>
+    <p v-for="num in 30" :key="num">{{ num }}</p>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      users: [
-        { name: 'name1', surn: 'surn1' },
-        { name: 'name2', surn: 'surn2' },
-        { name: 'name3', surn: 'surn3' }
-      ],
-      hrefs: [
-        { href: '1.html', text: 'text1' },
-        { href: '2.html', text: 'text2' },
-        { href: '3.html', text: 'text3' }
-      ],
-      products: [
-        { name: 'product1', price: 100, quantity: 5 },
-        { name: 'product2', price: 200, quantity: 4 },
-        { name: 'product3', price: 300, quantity: 3 }
-      ]
-    };
+    return {};
   }
 };
 </script>
