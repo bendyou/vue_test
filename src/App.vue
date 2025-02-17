95ff042cf04ce07903ee38bfd665db4395a03687
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:29:18 +0300

    Initial commit in task_42

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <p :class="{ valid: isValid, disabled: isDisabled }">text</p>
+  <div>
+    <h1>Task 1</h1>
+    <p :style="{ color: 'green', backgroundColor: 'yellow' }">
+      text
+    </p>
+
+    <h1>Task 2</h1>
+    <p :style="{ fontWeight: 'bold', fontStyle: 'italic' }">
+      text
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      isValid: true,
-      isDisabled: true
-    };
+    return {};
   }
 }
 </script>
-
-<style scoped>
-.valid {
-  color: green;
-}
-.disabled {
-  color: gray;
-}
-</style>
