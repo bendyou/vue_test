4495ed96b1f91c39c8c6625a1c2fdc6e7cace7d3
bendyou <vladmambetov46@gmail.com>
2025-02-17 03:57:26 +0300

    Initial commit in task_7

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
-  <!-- Task 1 -->
-  <div>{{ num * num }}</div>
+  <div>
+    <!-- Task 1 -->
+    <h1>Task 1</h1>
+    <p>{{ arr2[0] }}</p>
+    <p>{{ arr2[1] }}</p>
+    <p>{{ arr2[2] }}</p>
 
-  <!-- Task 2 -->
-  <div>{{ num1 + num2 + num3 }}</div>
+    <!-- Task 2 -->
+    <h1>Task 2</h1>
+    <p>{{ sum }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      num: 5,
-      num1: 1,
-      num2: 2,
-      num3: 3,
+      arr2: ['x', 'y', 'z'],
+      arr: [1, 2, 3]
+    };
+  },
+  computed: {
+    sum() {
+      return this.arr.reduce((acc, curr) => acc + curr, 0);
     }
   }
-}
+};
 </script>
+
+<style scoped>
+h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+p {
+  font-size: 18px;
+}
+</style>
