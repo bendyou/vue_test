efa803c1843fdd542f129473c3344cad1fdfd478
bendyou <vladmambetov46@gmail.com>
2025-02-17 03:49:07 +0300

    Initial commit in task_6

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <a :href="href">{{ text }}</a>
+  <!-- Task 1 -->
+  <div>{{ num * num }}</div>
+
+  <!-- Task 2 -->
+  <div>{{ num1 + num2 + num3 }}</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      text: 'page',
-      href: 'page.html',
+      num: 5,
+      num1: 1,
+      num2: 2,
+      num3: 3,
     }
   }
 }
