63c0782c472282491c246d890ee66da8638edf77
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:22:18 +0300

    Initial commit in task_33

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <h1>Task 1</h1>
-    <p v-for="num in 30" :key="num">{{ num }}</p>
+    <ul>
+      <template v-for="elem in items" :key="elem">
+        <li v-if="elem > 0">{{ elem }}</li>
+      </template>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      items: [1, -2, 3, -4, 5]
+    };
   }
 };
 </script>
