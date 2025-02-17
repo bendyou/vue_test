4bf0d651cf8125b6f843dee3012ef943962630e3
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:15:15 +0300

    Initial commit in task_25

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <p v-if="age < 18">teenager</p>
-  <p v-else-if="age >= 19 && age <= 25">young man</p>
-  <p v-else>man</p>
+  <button @click="hide">Hide</button>
+  <template v-if="isVisible">
+    <p>Text 1</p>
+    <p>Text 2</p>
+    <p>Text 3</p>
+  </template>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      age: 25,
+      isVisible: true,
     };
+  },
+  methods: {
+    hide() {
+      this.isVisible = false;
+    },
   },
 };
 </script>
