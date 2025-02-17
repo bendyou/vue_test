25cf1a98991567718a369ba6c15d501a8ec7d90a
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:18:58 +0300

    Initial commit in task_27

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
-  <button @click="toggle">Toggle</button>
-  <p v-show="isVisible">Text</p>
+  <div>
+    <h1>Task 1</h1>
+    <div v-for="item in items" :key="item">{{ item }}</div>
+    
+    <h1>Task 2</h1>
+    <div v-for="item in items" :key="item">{{ item * item }}</div>
+
+    <h1>Task 3</h1>
+    <ul>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      isVisible: true,
+      items: [1, 2, 3, 4, 5]
     };
-  },
-  methods: {
-    toggle() {
-      this.isVisible = !this.isVisible;
-    },
-  },
+  }
 };
 </script>
