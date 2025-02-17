e55f6aa30312a538c1fe70da0db0e872257bc4a4
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:05:32 +0300

    Initial commit in task_12

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,39 @@
   <div>
     <!-- Task 1 -->
     <h1>Task 1</h1>
-    <button @click="showSum">Show Sum</button>
+    <button @click="showDay">Show Day from Number</button>
+
+    <!-- Task 2 -->
+    <h1>Task 2</h1>
+    <button @click="showCurrentDay">Show Current Day</button>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      num1: 1,
-      num2: 2
-    };
+    return {};
   },
   methods: {
-    showSum() {
-      alert(this.num1 + this.num2);
+    // ¬спомогательный метод, который возвращает день недели по числу
+    getDayFromNumber(number) {
+      const days = [
+        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 
+        'Thursday', 'Friday', 'Saturday'
+      ];
+      return days[number];
+    },
+
+    // Task 1: ћетод, который выводит день недели по числу
+    showDay() {
+      let dayNumber = 3; // Ќапример, 3 - это среда (Wednesday)
+      alert(this.getDayFromNumber(dayNumber));
+    },
+
+    // Task 2: ќсновной метод, который выводит текущий день недели
+    showCurrentDay() {
+      const currentDay = new Date().getDay(); // ѕолучаем число текущего дн€ недели
+      alert(this.getDayFromNumber(currentDay)); // ¬ыводим название дн€
     }
   }
 };
