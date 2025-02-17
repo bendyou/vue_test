eabd5b30564b49ecad1e8c40aa19d54489c5d493
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:25:46 +0300

    Initial commit in task_35

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,39 @@
   <div>
     <h1>Task 1</h1>
     <ul>
-      <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+      <li v-for="elem in arr" :key="elem">{{ elem }}</li>
     </ul>
+    <button @click="addItem">Add Item</button>
+
+    <h1>Task 2</h1>
+    <ul>
+      <li v-for="elem in arr" :key="elem">{{ elem }}</li>
+    </ul>
+    <button @click="removeFirst">Remove First</button>
+
+    <h1>Task 3</h1>
+    <ul>
+      <li v-for="elem in arr" :key="elem">{{ elem }}</li>
+    </ul>
+    <button @click="removeLast">Remove Last</button>
+
+    <h1>Task 4</h1>
+    <ul>
+      <li v-for="elem in arr" :key="elem">{{ elem }}</li>
+    </ul>
+    <button @click="removeSecondLast">Remove Second Last</button>
+
+    <h1>Task 5</h1>
+    <ul>
+      <li v-for="elem in arr" :key="elem">{{ elem }}</li>
+    </ul>
+    <button @click="sortList">Sort List</button>
+
+    <h1>Task 6</h1>
+    <ul>
+      <li v-for="elem in arr" :key="elem">{{ elem }}</li>
+    </ul>
+    <button @click="reverseList">Reverse List</button>
   </div>
 </template>
 
@@ -11,12 +42,28 @@
 export default {
   data() {
     return {
-      products: [
-        { id: 1, name: 'product1' },
-        { id: 2, name: 'product2' },
-        { id: 3, name: 'product3' }
-      ]
+      arr: ['a', 'b', 'c'],
     };
+  },
+  methods: {
+    addItem() {
+      this.arr.push('new item');
+    },
+    removeFirst() {
+      this.arr.shift();
+    },
+    removeLast() {
+      this.arr.pop();
+    },
+    removeSecondLast() {
+      this.arr.splice(this.arr.length - 2, 1);
+    },
+    sortList() {
+      this.arr.sort();
+    },
+    reverseList() {
+      this.arr.reverse();
+    }
   }
-};
+}
 </script>
