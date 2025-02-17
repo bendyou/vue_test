b81cf67204f58e9ed4d32a614879b55e1ae2ae13
bendyou <vladmambetov46@gmail.com>
2025-02-17 04:35:28 +0300

    Initial commit in task_49

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-    <div>
-      <h1>Task 1</h1>
-      <div>
-        <input name="language" type="radio" v-model="choice" value="English"> English
-        <input name="language" type="radio" v-model="choice" value="Spanish"> Spanish
-        <input name="language" type="radio" v-model="choice" value="French"> French
-      </div>
-      <p>You choosed: {{ choice }}</p>
-  
-      <h1>Task 2</h1>
-      <div>
-        <input name="language" type="radio" v-model="choice" value="English"> English
-        <input name="language" type="radio" v-model="choice" value="Spanish"> Spanish
-        <input name="language" type="radio" v-model="choice" value="French"> French
-      </div>
-      <p v-if="choice === 'English'">Hello!</p>
-      <p v-if="choice === 'Spanish'">¡Hola!</p>
-      <p v-if="choice === 'French'">Bonjour!</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        choice: ''
-      };
-    }
-  };
-  </script>
-  
+  <div>
+    <h1>Task 1</h1>
+    <select v-model="selectedCity">
+      <option>New York</option>
+      <option>Los Angeles</option>
+      <option>Chicago</option>
+    </select>
+    <p>{{ selectedCity }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedCity: 'New York',
+    };
+  },
+};
+</script>
