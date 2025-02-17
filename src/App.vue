3a7df6111bb781e126f281f5874b4bc7a9272138
bendyou <vladmambetov46@gmail.com>
2025-02-17 03:43:47 +0300

    Initial commit in task_4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-    <div>
-      <h1>Hello, Vue!</h1>
-      <p>This is a simple example of a Vue component with text and tags.</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'App',
+  <div>{{ name }}</div>
+  <div>{{ surn }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'john',
+      surn: 'smit',
+    }
   }
-  </script>
-  
+}
+</script>
